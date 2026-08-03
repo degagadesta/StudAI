45866c1db0db7116e5f643ea62be3bb4b38fde18
degagadesta <dagagadasta09@gmail.com>
2026-08-03 13:55:39 +0300

    feat: initialize backend server structure

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,13 +1,30 @@
 import express from "express";
+import cors from "cors";
+import helmet from "helmet";
+import morgan from "morgan";
+import errorHandler from "./middlewares/errorHandler.js";
+
+import routes from "./routes/index.js";
 
 const app = express();
 
+// Global Middlewares
+app.use(cors());
+app.use(helmet());
+app.use(morgan("dev"));
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
-app.get("/", (req, res) => {
-    res.send("Backend is running");
+// Routes
+app.use("/api/v1", routes);
+
+// 404 Handler
+app.use((req, res) => {
+    res.status(404).json({
+        success: false,
+        message: "Route not found"
+    });
 });
 
-app.listen(3000, () => {
-    console.log("Server started");
-});
+app.use(errorHandler)
+export default app;
